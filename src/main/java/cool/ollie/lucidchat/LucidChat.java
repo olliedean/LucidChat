01bc95d5bbb01ff938810866811d4dab49d98eae
olliedean <olliedean@kakao.com>
2023-10-06 20:17:06 +0100

    removed vault dep

--- src/main/java/cool/ollie/lucidchat/LucidChat.java
+++ src/main/java/cool/ollie/lucidchat/LucidChat.java
@@ -3,26 +3,19 @@ package cool.ollie.lucidchat;
 import cool.ollie.lucidchat.commands.LCCommand;
 import cool.ollie.lucidchat.events.ChatListener;
 import cool.ollie.lucidchat.events.PlayerJoinLeaveEvents;
-import cool.ollie.lucidchat.managers.UserManager;
-import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 public final class LucidChat extends JavaPlugin {
     private static final Logger log = Logger.getLogger("Minecraft");
-    private static Economy econ = null;
     public boolean isPAPIEnabled = false;
 
     @Override
     public void onEnable() {
-        if(!setupEconomy()) {
-            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
         if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
             isPAPIEnabled = true;
             log.info(String.format("[%s] - PlaceholderAPI found, enabling placeholders!", getDescription().getName()));
@@ -37,26 +30,33 @@ public final class LucidChat extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerJoinLeaveEvents(), this);
         this.getCommand("lucidchat").setExecutor(new LCCommand());
 
+        // auto announcer
+        int delay = getConfig().getInt("auto-announcer.interval");
+        if(delay > 0) {
+            if(getConfig().getList("auto-announcer.messages").size() == 0) {
+                log.info(String.format("[%s] - Auto announcer is enabled but no messages are set!", getDescription().getName()));
+                log.info(String.format("[%s] - Disabling auto announcer.", getDescription().getName()));
+                getConfig().set("auto-announcer.interval", 0);
+                saveConfig();
+                return;
+            }
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+                @Override
+                public void run() {
+                    List messages = getConfig().getList("auto-announcer.messages");
+                    int random = new Random().nextInt(messages.size());
+                    String message = messages.get(random).toString();
+                    message = getConfig().getString("auto-announcer.prefix") + message;
+                    message = message.replace("&", "§");
+                    // still need to add support for placeholders
+                    Bukkit.broadcastMessage(message);
+                }
+            }, 0, delay * 20L);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return true;
-    }
-
-    public static Economy getEconomy() {
-        return econ;
     }
 }
